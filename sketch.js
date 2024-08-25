e8f0b2e33f671c0971cf60bc74b6742b03853909
Edson1227 <00001097409570sp@al.educacao.sp.gov.br>
2024-08-25 10:44:54 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+  background("black");
+  }
+  function draw() {
+    
+  }
+  function draw(){
+    stroke("#0FA8EE");
+    fill("yellow");
+    //cons#0788C2og(mouseIsPressed);
+    if (mouseIsPressed){
+      rect(mouseX,mouseY, 10, 15);
+    }
+    
+  }
